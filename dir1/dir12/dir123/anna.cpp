48ccfdfe8b445111585fbdc0b9b07932f0eae3e0
Soloveva Anna <anna.soloveva@jetbrains.com>
2017-12-28 10:54:22 +0300

    first

---
+++ dir1/dir12/dir123/anna.cpp
@@ -0,0 +1,5 @@
+//
+// Created by jetbrains on 28/12/2017.
+//
+
+#include "anna.h"
---
+++ dir1/dir12/dir123/anna.h
@@ -0,0 +1,8 @@
+//
+// Created by jetbrains on 28/12/2017.
+//
+
+#ifndef UNTITLED24_ANNA_H
+#define UNTITLED24_ANNA_H
+
+#endif //UNTITLED24_ANNA_H
