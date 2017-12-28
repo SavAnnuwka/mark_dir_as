2922186eca194cf59fbb62b78dc0dba0f3bbfcae
Soloveva Anna <anna.soloveva@jetbrains.com>
2017-12-28 11:09:49 +0300

    message

---
+++ cmake-build-debug/CMakeFiles/untitled24.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/untitled24.dir/main.cpp.o"
+  "CMakeFiles/untitled24.dir/dir1/dir12/dir123/anna.cpp.o"
+  "untitled24.pdb"
+  "untitled24"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/untitled24.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
